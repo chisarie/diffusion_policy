91cbff504851a69e10a26960e90aa7e2c40e312a
Eugenio <chisari@cs.uni-freiburg.de>
2024-04-29 17:22:25 +0200

    make timeconditioning optional

--- diffusion_policy/model/diffusion/conditional_unet1d.py
+++ diffusion_policy/model/diffusion/conditional_unet1d.py
@@ -172,7 +172,7 @@ class ConditionalUnet1D(nn.Module):
 
     def forward(self, 
             sample: torch.Tensor, 
-            timestep: Union[torch.Tensor, float, int], 
+            timestep: Union[torch.Tensor, float, int]=None, 
             local_cond=None, global_cond=None, **kwargs):
         """
         x: (B,T,input_dim)
@@ -184,21 +184,27 @@ class ConditionalUnet1D(nn.Module):
         sample = einops.rearrange(sample, 'b h t -> b t h')
 
         # 1. time
-        timesteps = timestep
-        if not torch.is_tensor(timesteps):
-            # TODO: this requires sync between CPU and GPU. So try to pass timesteps as tensors if you can
-            timesteps = torch.tensor([timesteps], dtype=torch.long, device=sample.device)
-        elif torch.is_tensor(timesteps) and len(timesteps.shape) == 0:
-            timesteps = timesteps[None].to(sample.device)
-        # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
-        timesteps = timesteps.expand(sample.shape[0])
+        if timestep is not None:
+            timesteps = timestep
+            if not torch.is_tensor(timesteps):
+                # TODO: this requires sync between CPU and GPU. So try to pass timesteps as tensors if you can
+                timesteps = torch.tensor([timesteps], dtype=torch.long, device=sample.device)
+            elif torch.is_tensor(timesteps) and len(timesteps.shape) == 0:
+                timesteps = timesteps[None].to(sample.device)
+            # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
+            timesteps = timesteps.expand(sample.shape[0])
 
-        global_feature = self.diffusion_step_encoder(timesteps)
+            global_feature = self.diffusion_step_encoder(timesteps)
 
-        if global_cond is not None:
-            global_feature = torch.cat([
-                global_feature, global_cond
-            ], axis=-1)
+            if global_cond is not None:
+                global_feature = torch.cat([
+                    global_feature, global_cond
+                ], axis=-1)
+        elif global_cond is not None:
+            # For this, remember to initialize diffusion_step_embed_dim=0
+            global_feature = global_cond
+        else:
+            raise ValueError("Either timestep or global_cond must be provided")
         
         # encode local features
         h_local = list()
